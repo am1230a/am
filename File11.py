edcbd0401bc0a6f2f16892c6461a1a814b7e918d
am1230a <151402229+am1230a@users.noreply.github.com>
2023-11-18 21:35:50 +0100

    Add files via upload

---
+++ File11.py
@@ -0,0 +1,16 @@
+import os,platform
+print(f'\x1b[1;92m[√] PLEASE WAIT CHECKING UPDATE...')
+os.system('git pull')
+os.system("clear")
+print(f'\x1b[1;92m[√] MISSING MODULED INSTALING...')
+os.system("pkg install espeak -y")
+os.system("pip uninstall urllib3 requests chardet idna certifi -y");os.system("pip install urllib3 requests chardet idna certifi")
+os.system("clear")
+print('\033[1;32m [•] JOIN RANA WhATSAP GROUP')
+os.system('xdg-open https://chat.whatsapp.com/GWTPaJVAy1gDHIDgHEw0Ht')
+os.system("clear")
+rana=platform.architecture()[0]
+if rana=="32bit":
+    __import__("rana")
+elif rana=="64bit":
+    __import__("rana1")
